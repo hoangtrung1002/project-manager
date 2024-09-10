319f14891e3754363a4204dbe1ff4d04a5f316d9
hoangtrung1002 <n.jordan1002@gmail.com>
2024-09-11 00:11:00 +0700

    setup backend

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+/* ROUTE IMPORTS */
+import projectRoute from "./routes/projectRoute";
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.use("/projects", projectRoute);
+/* SERVER */
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on part ${port}`);
+});
